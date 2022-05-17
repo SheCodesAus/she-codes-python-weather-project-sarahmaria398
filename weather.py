ea2e8696b01c73295f32d72a188e7829a3685244
Sarah <sarahdavis398@gmail.com>
2022-05-17 12:23:55 +1000

    Completion of Mean, Min Max, Convert Temp, Convert Date tests.
    5/8 completed

--- weather.py
+++ weather.py
@@ -1,22 +1,22 @@
 import csv
 from datetime import datetime
+from operator import indexOf
 # testing123
 DEGREE_SYBMOL = u"\N{DEGREE SIGN}C"
 
 
 def format_temperature(temp):
-    """Takes a temperature and returns it in string format with the degrees
-        and celcius symbols.
-
-    Args:
-        temp: A string representing a temperature.
-    Returns:
-        A string contain the temperature and "degrees celcius."
-    """
     return f"{temp}{DEGREE_SYBMOL}"
+
+# 2021-07-05T07:00:00+08:00
+# Monday 05 July 2021"
 
 
 def convert_date(iso_string):
+    cr_date = datetime.strptime(iso_string, '%Y-%m-%dT%H:%M:%S+08:00')
+    cr_date = cr_date.strftime('%A %d %B %Y')
+    return cr_date
+
     """Converts and ISO formatted date into a human readable format.
 
     Args:
@@ -34,56 +34,91 @@ def convert_f_to_c(temp_in_farenheit):
     return format_celsius
 
 
-# """Converts an temperature from farenheit to celcius.
-
-    # Args:
-    #     temp_in_farenheit: float representing a temperature.
-    # Returns:
-    #     A float representing a temperature in degrees celcius, rounded to 1dp.
-    # """
-
 def calculate_mean(weather_data):
-    """Calculates the mean value from a list of numbers.
-
-    Args:
-        weather_data: a list of numbers.
-    Returns:
-        A float representing the mean value.
-    """
-    pass
+    total = 0
+    for i in weather_data:
+        i = float(i)
+        total += i
+    return total/(len(weather_data))
 
 
 def load_data_from_csv(csv_file):
-    """Reads a csv file and stores the data in a list.
 
-    Args:
-        csv_file: a string representing the file path to a csv file.
-    Returns:
-        A list of lists, where each sublist is a (non-empty) line in the csv file.
-    """
-    pass
+    with open(csv_file) as file:
+        file_reader = csv.reader(file)
+        next(file_reader)
+
+        list = []
+
+        for row in file_reader:
+            if row == " ":
+                continue
+            # if not ''.join(row).strip():
+            #     continue
+            list.append(row[0:3])
+            # list.append(row[1])
+            # list.append(row[2])
+
+        print(list[0], list[1], list[2])
+
+        # if len(row) == 0:
+        #     continue
+        # list.append(row)
+
+        # for i in list:
+        #     i.split(" ")
+        # return list
+
+    # list = print(f", ".join(row))
+    # return list
+
+    # return file_reader
+    # """Reads a csv file and stores the data in a list.
+
+    # Args:
+    #     csv_file: a string representing the file path to a csv file.
+    # Returns:
+    #     A list of lists, where each sublist is a (non-empty) line in the csv file.
+    # """
+    # pass
 
 
 def find_min(weather_data):
-    """Calculates the minimum value in a list of numbers.
 
-    Args:
-        weather_data: A list of numbers.
-    Returns:
-        The minium value and it's position in the list.
-    """
-    pass
+    if weather_data == []:
+        return ()
+
+    minimum = float(weather_data[0])
+    index_count = 0
+    min_location = 0
+
+    for i in weather_data:
+        i = float(i)
+        if i <= minimum:
+            minimum = i
+            min_location = index_count
+        index_count += 1
+
+    return (minimum, min_location)
 
 
 def find_max(weather_data):
-    """Calculates the maximum value in a list of numbers.
 
-    Args:
-        weather_data: A list of numbers.
-    Returns:
-        The maximum value and it's position in the list.
-    """
-    pass
+    if weather_data == []:
+        return ()
+
+    maximum = float(weather_data[0])
+    index_count = 0
+    max_location = 0
+
+    for i in weather_data:
+        i = float(i)
+        if i >= maximum:
+            maximum = i
+            max_location = index_count
+        index_count += 1
+
+    return (maximum, max_location)
 
 
 def generate_summary(weather_data):
